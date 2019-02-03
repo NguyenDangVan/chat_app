8bb62f27b772823060f611970a30dd71661ff04b
Dang Van <dinvvan@gmail.com>
2019-02-03 21:04:12 +0700

    user create new group

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,4 +1,9 @@
 class ConversationsController < ApplicationController
+  def index
+    @users = User.find_friend
+    @conversations = Conversation.includes(:recipent, :sender)
+  end
+
   def create
     @conversation = Conversation.get current_user.id, params[:user_id]
     @messages = @conversation.messages
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   root "static_pages#home"
   devise_for :users, controllers: {sessions: "sessions"}
-  resources :users
+  resources :users do
+    resources :groups do
+      resources :roooms
+    end
+  end
   resources :conversations do
     resources :messages
   end
